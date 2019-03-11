9c81ffbbbc94d2d83de33104b55e02fa92fd80f1
Headturna <suraj-sharma@live.com>
2019-03-11 15:21:32 +0100

    TextureSelector GUI element

--- SFML_RPG/Gui.cpp
+++ SFML_RPG/Gui.cpp
@@ -237,3 +237,42 @@ void gui::DropDownList::render(sf::RenderTarget & target)
 		}
 	}
 }
+
+//TEXTURE SELECTOR ===================================================
+gui::TextureSelector::TextureSelector(float x, float y, float width, float height, const sf::Texture* texture_sheet)
+{
+	this->bounds.setSize(sf::Vector2f(width, height));
+	this->bounds.setPosition(x, y);
+	this->bounds.setFillColor(sf::Color(50, 50, 50, 100));
+	this->bounds.setOutlineThickness(1.f);
+	this->bounds.setOutlineColor(sf::Color(255, 255, 255, 200));
+
+	this->sheet.setTexture(*texture_sheet);
+	this->sheet.setPosition(x, y);
+
+	if (this->sheet.getGlobalBounds().width > this->bounds.getGlobalBounds().width)
+	{
+		this->sheet.setTextureRect(sf::IntRect(0, 0, this->bounds.getGlobalBounds().width, this->sheet.getGlobalBounds().height));
+	}
+	if (this->sheet.getGlobalBounds().height > this->bounds.getGlobalBounds().height)
+	{
+		this->sheet.setTextureRect(sf::IntRect(0, 0, this->sheet.getGlobalBounds().width, this->bounds.getGlobalBounds().height));
+	}
+}
+
+gui::TextureSelector::~TextureSelector()
+{
+
+}
+
+//Functions
+void gui::TextureSelector::update()
+{
+
+}
+
+void gui::TextureSelector::render(sf::RenderTarget& target)
+{
+	target.draw(this->bounds);
+	target.draw(this->sheet);
+}
