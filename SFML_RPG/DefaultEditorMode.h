5be5778dd0c01b9c9a6732cbc2ed0fe744407194
Headturna <suraj-sharma@live.com>
2019-05-25 22:13:23 +0200

    Working on editor state and default mode...

--- SFML_RPG/DefaultEditorMode.h
+++ SFML_RPG/DefaultEditorMode.h
@@ -6,13 +6,36 @@
 class State;
 class StateData;
 class EditorMode;
+class TileMap;
+class Tile;
 
 class DefaultEditorMode :
 	public EditorMode
 {
+private:
+	sf::Text cursorText;
+	sf::RectangleShape sidebar;
+	sf::RectangleShape selectorRect;
+	gui::TextureSelector* textureSelector;
+	sf::IntRect textureRect;
+	bool collision;
+	short type;
+	int layer;
+	bool tileAddLock;
+
+	void initVariables();
+	void initGui();
+
 public:
-	DefaultEditorMode(StateData* state_data);
+	DefaultEditorMode(StateData* state_data, TileMap* tile_map, sf::Font* font);
 	virtual ~DefaultEditorMode();
+
+	void updateInput(const float& dt);
+	void updateGui(const float& dt);
+	void update(const float& dt);
+
+	void renderGui(sf::RenderTarget& target);
+	void render(sf::RenderTarget* target = NULL);
 };
 
 #endif //!DEFAULTEDITORMODE_H
